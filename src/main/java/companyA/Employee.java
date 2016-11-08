0edcf9a2bce07f4f3c2b8efaa455e66a7a8bd4c6
Evan Thompson <ethom7@uis.edu>
2016-11-08 00:27:09 -0600

    Update Employee.java

--- src/main/java/companyA/Employee.java
+++ src/main/java/companyA/Employee.java
@@ -1,139 +1,196 @@
 package companyA;
 
-import java.util.Date;
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+import org.joda.time.format.DateTimeFormatter;
+import org.joda.time.format.ISODateTimeFormat;
 
-public class Employee {
-	
-	private int employeeId;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * Created by evanpthompson on 11/1/2016.
+ *
+ * Dates are contained as Strings to ensure proper serialization and deserialization
+ * of the ISODate object as stored in the mongodb because jackson does not support the format.
+ */
+
+@XmlRootElement(name = "employee")
+public class Employee implements Comparable<Employee> {
+
+	private int id;
+	private Object _id;
 	private String firstName;
 	private String middleName;
 	private String lastName;
-	private int socialSecurityNumber;
-	private String dateOfBirth;
+	private String givenName;
+	private String socialSecurityNumber;
+	private String dob;
 	private PostalAddress postalAddress;
-	private EmergencyContact emergencyContact;
 	private String phoneNumber;
-	private boolean isActive;
-	
-	
-	public boolean isActive() {
-		return isActive;
+	private EmergencyContact emergencyContact;
+	private String hireDate;
+	private DirectReports directReports;
+	private boolean activeEmployee;
+
+	public Employee() {     }
+
+	@XmlElement
+	public int getId() {
+		return id;
 	}
 
-	public void setActive(boolean isActive) {
-		this.isActive = isActive;
+	public void setId(int id) {
+		this.id = id;
 	}
 
-	public Employee() {  }
-	
-	public Employee(String firstName, String middleName, String lastName, int socialSecurityNumber, String dateOfBirth,
-			PostalAddress postalAddress, EmergencyContact emergencyContact, String phoneNumber) {
-		
-		
-		this.firstName = firstName;
-		this.middleName = middleName;
-		this.lastName = lastName;
-		this.socialSecurityNumber = socialSecurityNumber;
-		this.dateOfBirth = dateOfBirth;
-		this.postalAddress = postalAddress;
-		this.emergencyContact = emergencyContact;
-		this.phoneNumber = phoneNumber;
+	public Object get_id() {
+		return _id;
 	}
 
+	public void set_id(Object _id) {
+		this._id = _id;
+	}
 
+	@XmlElement
 	public String getFirstName() {
 		return firstName;
 	}
 
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
 
+	@XmlElement
 	public String getMiddleName() {
 		return middleName;
 	}
 
+	public void setMiddleName(String middleName) {
+		this.middleName = middleName;
+	}
 
+	@XmlElement
 	public String getLastName() {
 		return lastName;
 	}
 
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
 
-	public int getSocialSecurityNumber() {
+	@XmlElement
+	public String getGivenName() {
+		return givenName;
+	}
+
+	public void setGivenName(String givenName) {
+		this.givenName = givenName;
+	}
+
+	@XmlElement
+	public String getSocialSecurityNumber() {
 		return socialSecurityNumber;
 	}
 
-
-	public String getDateOfBirth() {
-		return dateOfBirth;
+	public void setSocialSecurityNumber(String socialSecurityNumber) {
+		this.socialSecurityNumber = socialSecurityNumber;
 	}
 
+	@XmlElement
+	public String getDob() {
+		return dob;
+	}
 
+	public void setDob(String dob) {
+		this.dob = dob;
+	}
+
+	@XmlElement
 	public PostalAddress getPostalAddress() {
 		return postalAddress;
 	}
 
-
-	public EmergencyContact getEmergencyContact() {
-		return emergencyContact;
+	public void setPostalAddress(PostalAddress postalAddress) {
+		this.postalAddress = postalAddress;
 	}
 
-
+	@XmlElement
 	public String getPhoneNumber() {
 		return phoneNumber;
 	}
 
-
-	void setFirstName(String firstName) {
-		this.firstName = firstName;
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
 	}
 
-
-	void setMiddleName(String middleName) {
-		this.middleName = middleName;
+	@XmlElement
+	public EmergencyContact getEmergencyContact() {
+		return emergencyContact;
 	}
 
-
-	void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-
-
-	void setSocialSecurityNumber(int socialSecurityNumber) {
-		this.socialSecurityNumber = socialSecurityNumber;
-	}
-
-
-	void setDateOfBirth(String dateOfBirth) {
-		this.dateOfBirth = dateOfBirth;
-	}
-
-
-	void setPostalAddress(PostalAddress postalAddress) {
-		this.postalAddress = postalAddress;
-	}
-
-
-	private void setEmergencyContact(EmergencyContact emergencyContact) {
+	public void setEmergencyContact(EmergencyContact emergencyContact) {
 		this.emergencyContact = emergencyContact;
 	}
 
+	@XmlElement
+	public String getHireDate() {
+		return hireDate;
+	}
 
-	void setPhoneNumber(String phoneNumber) {
-		this.phoneNumber = phoneNumber;
+	public void setHireDate(String hireDate) {
+		this.hireDate = hireDate;
 	}
-	
-	public String fullNameAsString() {
-		return String.format("%s %s %s", firstName, middleName, lastName);
+
+	@XmlElement
+	public DirectReports getDirectReports() {
+		return directReports;
 	}
-		
-	
+
+	public void setDirectReports(DirectReports directReports) {
+		this.directReports = directReports;
+	}
+
+	@XmlElement
+	public boolean isActiveEmployee() {
+		return activeEmployee;
+	}
+
+	public void setActiveEmployee(boolean activeEmployee) {
+		this.activeEmployee = activeEmployee;
+	}
+
+	public int compareTo(Employee other) {
+		return this.id - other.id;
+	}
+
+	// method will return a timestamp as a string for a set year, month, and day. used for dob and hiredate
+	public String dateTimestamp(int dobYear, int dobMonth, int dobDay) {
+		DateTime dob = new DateTime(dobYear, dobMonth, dobDay, 0, 0, DateTimeZone.UTC);
+		DateTimeFormatter formatter = ISODateTimeFormat.dateTime();
+		String dobTimestamp = formatter.print(dob);
+
+		// test of dobTimeStamp
+		return dobTimestamp;
+	}
+
 	@Override
 	public String toString() {
-		return String.format("%s %s %s: phone number: %s\n%s\nEmergency Contact: %s", firstName, middleName, lastName, phoneNumber, postalAddress,emergencyContact);
+		return "Employee : {" +
+				"id : " + id +
+				", _id : " + _id +
+				", firstName : " + firstName +
+				", middleName : " + middleName +
+				", lastName : " + lastName +
+				", givenName : " + givenName +
+				", socialSecurityNumber : " + socialSecurityNumber +
+				", dob : " + dob +
+				", postalAddress : " + postalAddress +
+				", phoneNumber : " + phoneNumber +
+				", emergencyContact : " + emergencyContact +
+				", hireDate : " + hireDate +
+				", directReports : " + directReports +
+				", activeEmployee : " + activeEmployee +
+				'}';
 	}
-
-	
-
-
-	
-	
-	
 }
